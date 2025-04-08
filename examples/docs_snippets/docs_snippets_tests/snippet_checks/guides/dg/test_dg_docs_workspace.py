3ed5e461bc8f922fdeae4633ffff1a085c43c1fc
Sean Mackesey <s.mackesey@gmail.com>
2025-04-08 17:37:59 -0400

    [dg] Make init command work with a dirname arg

--- examples/docs_snippets/docs_snippets_tests/snippet_checks/guides/dg/test_dg_docs_workspace.py
+++ examples/docs_snippets/docs_snippets_tests/snippet_checks/guides/dg/test_dg_docs_workspace.py
@@ -32,7 +32,7 @@ def test_dg_docs_workspace(update_snippets: bool) -> None:
     with isolated_snippet_generation_environment() as get_next_snip_number:
         # Scaffold workspace
         run_command_and_snippet_output(
-            cmd='echo "project-1\n" | dg init --use-editable-dagster --workspace-name dagster-workspace',
+            cmd='echo "project-1\n" | dg init --use-editable-dagster --workspace dagster-workspace',
             snippet_path=DG_SNIPPETS_DIR / f"{get_next_snip_number()}-dg-init.txt",
             update_snippets=update_snippets,
             snippet_replace_regex=[
